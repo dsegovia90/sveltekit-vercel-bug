e1a116c6617915d972b05e2e4957012248ac88f1
Daniel Segovia <segoarce90@gmail.com>
2021-04-05 19:29:12 -0700

    if browser

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { browser } from '$app/env'
 	import ably from "ably"
 	// just so i don't have unused import warnings
-	ably
+	if (browser) {
+		ably
+	}
 </script>
 
 <main>
